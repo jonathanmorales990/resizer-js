bf0b583ce134653e40e861bb1783dd447becfc30
jonathanmorales990 <johnmorales990@gmail.com>
2019-09-01 14:04:13 -0300

    Added offset

--- dist/resizer.js
+++ dist/resizer.js
@@ -6,7 +6,14 @@ resizer = {
 	original_mouse_x: 0,
 	original_mouse_y: 0,
 	minimum_size: 20,
-	element: null
+	maxWidth: 0,
+	element: null,
+	parent: {
+		paddingLeft: 0,
+		paddingTop: 0,
+		marginLeft: 0,
+		marginTop: 0
+	}
 }
 resizer.applyResizer = function (element) {
 	element.style.position = 'absolute';
@@ -15,11 +22,17 @@ resizer.applyResizer = function (element) {
 	this.onMoveEvent(element);
 }
 resizer.onResizeEvent = function (event) {
+	var maxLeft = resizer.parent.paddingLeft + resizer.parent.marginLeft;
+	var maxTop = resizer.parent.paddingTop + resizer.parent.marginTop;
+	var maxWidth = resizer.maxWidth - resizer.original_x;
 	if (resizer.hasClass(resizer.currect_corner, 'bottom-right')) {
 		var width = resizer.original_width + (event.pageX - resizer.original_mouse_x);
 		var height = resizer.original_height + (event.pageY - resizer.original_mouse_y);
 		if (width > resizer.minimum_size) {
-			resizer.element.style.width = width + 'px';
+			if(width <= maxWidth)
+				resizer.element.style.width = width + 'px';
+			else
+				resizer.element.style.width = maxWidth + 'px';
 		}
 		if (height > resizer.minimum_size) {
 			resizer.element.style.height = height + 'px';
@@ -33,9 +46,12 @@ resizer.onResizeEvent = function (event) {
 		}
 		if (width > resizer.minimum_size ) {
 			var left_calc = (resizer.original_x + (event.pageX - resizer.original_mouse_x));
-			if (left_calc > 0) {
+			if (left_calc >= maxLeft) {
 				resizer.element.style.width = width + 'px';
 				resizer.element.style.left = left_calc + 'px';
+			} else {
+				resizer.element.style.width = ((width + left_calc) - maxLeft) + 'px';
+				resizer.element.style.left = maxLeft + 'px';
 			}
 		}
 	}
@@ -44,13 +60,19 @@ resizer.onResizeEvent = function (event) {
 		var height = resizer.original_height - (event.pageY - resizer.original_mouse_y);
 		if (height > resizer.minimum_size) {
 			var top_calc = (resizer.original_y + (event.pageY - resizer.original_mouse_y));
-			if (top_calc > 0) {
+			if (top_calc > maxTop) {
 				resizer.element.style.height = height + 'px';
 				resizer.element.style.top = top_calc + 'px';
+			} else {
+				resizer.element.style.height = ((height + top_calc) - maxTop) + 'px';
+				resizer.element.style.top = maxTop + 'px';
 			}
 		}
 		if (width > resizer.minimum_size ) {
-			resizer.element.style.width = width + 'px';
+			if(width <= maxWidth)
+				resizer.element.style.width = width + 'px';
+			else
+				resizer.element.style.width = maxWidth + 'px';
 		}
 	}
 	if (resizer.hasClass(resizer.currect_corner, 'top-left')) {
@@ -58,16 +80,22 @@ resizer.onResizeEvent = function (event) {
 		var height = resizer.original_height - (event.pageY - resizer.original_mouse_y);
 		if (width > resizer.minimum_size ) {
 			var left_calc = (resizer.original_x + (event.pageX - resizer.original_mouse_x));
-			if (left_calc > 0) {
+			if (left_calc >= maxLeft) {
 				resizer.element.style.width = width + 'px';
-				resizer.element.style.left = left_calc + 'px'; 
+				resizer.element.style.left = left_calc + 'px';
+			} else {
+				resizer.element.style.width = ((width + left_calc) - maxLeft) + 'px';
+				resizer.element.style.left = maxLeft + 'px';
 			}
 		}
 		if (height > resizer.minimum_size) {
 			var top_calc = (resizer.original_y + (event.pageY - resizer.original_mouse_y));
-			if (top_calc > 0) {
+			if (top_calc > maxTop) {
 				resizer.element.style.height = height + 'px';
 				resizer.element.style.top = top_calc + 'px';
+			} else {
+				resizer.element.style.height = ((height + top_calc) - maxTop) + 'px';
+				resizer.element.style.top = maxTop + 'px';
 			}
 		}		
 	}
@@ -112,6 +140,11 @@ resizer.onMoveEvent = function (element) {
 			resizer.original_height = parseFloat(getComputedStyle(element, null).getPropertyValue('height').replace('px', ''));
 			resizer.original_x = parseFloat(getComputedStyle(element, null).getPropertyValue('left').replace('px', ''));
 			resizer.original_y = parseFloat(getComputedStyle(element, null).getPropertyValue('top').replace('px', ''));
+			resizer.maxWidth = parseFloat(getComputedStyle(element.parentNode, null).getPropertyValue('width').replace('px', ''));
+			resizer.parent.paddingLeft = parseFloat(getComputedStyle(resizer.element.parentNode, null).getPropertyValue('padding-left').replace('px', ''));
+			resizer.parent.paddingTop = parseFloat(getComputedStyle(resizer.element.parentNode, null).getPropertyValue('padding-top').replace('px', ''));
+			resizer.parent.marginLeft = parseFloat(getComputedStyle(resizer.element.parentNode, null).getPropertyValue('margin-left').replace('px', ''));
+			resizer.parent.marginTop = parseFloat(getComputedStyle(resizer.element.parentNode, null).getPropertyValue('margin-top').replace('px', ''));
 			resizer.original_mouse_x = event.pageX;
 			resizer.original_mouse_y = event.pageY;
 			resizer.currect_corner = event.target;
